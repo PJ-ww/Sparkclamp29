466c8f418d71d4a744532ac61397f6b712c5ad61
Pathanan <Pathanan.ban@dome.tu.ac.th>
2026-06-29 11:39:33 +0700

    Read_exel_file

--- pipline/ingest.py
+++ pipline/ingest.py
@@ -1,1 +1,12 @@
 import pandas as pd
+
+# อ่าน Excel
+df = pd.read_excel("data/thammasat_workshop_dataset.xlsx")
+
+print(df.head())
+print(df.info())
+
+# Export Raw
+df.to_csv("output/raw_workshop_data.csv", index=False)
+
+print("Raw layer created")
